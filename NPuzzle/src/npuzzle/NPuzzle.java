ff9318d851f05fc33d195e2d0a13966e14664be1
gusamasan <gusamasan@ab5189cc-a75d-1527-ca4c-538d297905de>
2010-08-21 02:00:31 +0000

    Se modificaron las clases: NPuzzle y Casilla

--- NPuzzle/src/npuzzle/NPuzzle.java
+++ NPuzzle/src/npuzzle/NPuzzle.java
@@ -12,16 +12,59 @@ import busqueda.*;
  * @author gusamasan
  */
 public class NPuzzle implements Problema{
+
+    private int cantidadFilasTablero;
+
+    private CasillaPuzzle   casillaVacia,
+                            tablero[][];
+
+
+
     public NPuzzle(){
         
     }
 
 
-    public void inicializar( Estrategia unaEstrategia ){
-        // buscar ubicaciOn del casillero blanco
+    public int getCantidadFilasTablero(){
+        return( this.cantidadFilasTablero );
+    }
 
-        // hacer copia, si hace falta, de la estructura de deatos que representa el tablero
-        // de nUmeros
+    public void setCantidadFilasTablero( int pCantidadFilasTablero ){
+        this.cantidadFilasTablero   = pCantidadFilasTablero;
+    }
+
+    public CasillaPuzzle[][] getTablero(){
+        return( null );
+    }
+
+    public void setTablero( CasillaPuzzle[][] pTablero ){
+        this.tablero    = pTablero;
+    }
+
+    public void inicializar( Estrategia unaEstrategia ){
+    // -------------------------------------------------------------------------
+
+        int fila, columna, menor;
+    
+    // -------------------------------------------------------------------------
+
+        this.casillaVacia   = new CasillaPuzzle();
+
+    
+            for( fila= 0; fila < this.cantidadFilasTablero; fila++ ){
+
+                for( columna=0; columna < this.cantidadFilasTablero; columna++ ){
+
+                    if( this.tablero[ fila ][ columna ].obtenerValor() == null ){ // es la casilla vacIa
+                        this.casillaVacia = this.tablero[ fila ][ columna ];
+                        break;
+                    }
+                }
+
+            }
+
+        this.expandir( this.casillaVacia, unaEstrategia );
+        
     }
 
     public void expandir( Nodo nodoActual, Estrategia unaEstrategia ){
@@ -33,10 +76,59 @@ public class NPuzzle implements Problema{
 
       
         // expandir el nodo actual: para cada hijo, ejecutar la LINEA siguiente
-        unaEstrategia.insertarNodo( nodoHijo );
+        //unaEstrategia.insertarNodo( nodoHijo );
     }
 
+
+    private void moverCasillaVacia( CasillaPuzzle nodoActual ){
+    // ----------------------------------------------------------------------
+
+
+
+    // ----------------------------------------------------------------------
+
+
+        this.casillaVacia.insertarValor( nodoActual.obtenerValor() );
+        this.casillaVacia   = nodoActual;
+        this.casillaVacia.insertarValor( null );
+    }
+
+
+    private boolean elTableroEstaOrdenado(){
+    // -------------------------------------------------------------------------
+
+        int fila, columna, menor;
+        int valorActual;
+
+    // -------------------------------------------------------------------------
+        
+        menor   = ( ( Integer ) this.tablero[ 0 ][ 0 ].obtenerValor() ).intValue();
+
+        for( fila= 0; fila < this.cantidadFilasTablero; fila++ ){
+
+            for( columna=0; columna < this.cantidadFilasTablero; columna++ ){
+
+                valorActual = ( ( Integer ) this.tablero[ fila ][ columna ].obtenerValor() ).intValue();
+
+                if( valorActual < menor )
+                    return(  false );
+                else
+                    menor   = valorActual;
+
+            }
+
+        }
+
+        return( true );
+    }
+
+    
     public boolean esNodoMeta( Nodo nodoActual ){
-        return( false );
+    
+        this.moverCasillaVacia(( CasillaPuzzle ) nodoActual );
+
+        return( elTableroEstaOrdenado() );
+
+        
     }
 }
